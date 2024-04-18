0d26feb2fc95f0bb32fe03d2a89ab9a0f95278dd
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2024-04-18 13:23:54 +0300

    create sign out buttun and it is working.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  void logOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: logOut, icon: const Icon(Icons.logout_outlined))
+        ],
+      ),
+    );
   }
 }
